6c13a07f46f1b314850385023325bbafbf7cc663
jcl <chaelynjang@lotte.net>
2023-11-15 17:22:42 +0900

    Aspect에 매개변수 전달 방법 확인

--- src/main/java/hello/aop/member/MemberServiceImpl.java
+++ src/main/java/hello/aop/member/MemberServiceImpl.java
@@ -2,14 +2,17 @@ package hello.aop.member;
 
 import hello.aop.member.annotation.ClassAop;
 import hello.aop.member.annotation.MethodAop;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @ClassAop
 @Component
+@Slf4j
 public class MemberServiceImpl implements MemberService{
     @Override
     @MethodAop("test value")
     public String hello(String param) {
+        log.info("param = {}", param);
         return "ok";
     }
 
